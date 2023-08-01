deedd5f7cb18ffd233e49aa216405eeaca78dfde
ramya1907 <ramya.srinivasan1907@gmail.com>
2023-08-01 16:15:36 -0400

    Add new component stats

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { ViewComponent } from './view/view.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ViewComponent
+    ViewComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
