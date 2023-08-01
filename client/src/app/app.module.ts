ae5745e259d9df992e8e5efb7b5dca6bf1b89bb1
ramya1907 <ramya.srinivasan1907@gmail.com>
2023-08-01 15:17:03 -0400

    Remove last-fm-callback component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { ViewComponent } from './view/view.component';
-import { LastFmCallbackComponentComponent } from './last-fm-callback-component/last-fm-callback-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ViewComponent,
-    LastFmCallbackComponentComponent
+    ViewComponent
   ],
   imports: [
     BrowserModule,
