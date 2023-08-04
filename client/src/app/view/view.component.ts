69b08e6f719c0e896909d8d622a315d544c0a397
ramya1907 <ramya.srinivasan1907@gmail.com>
2023-08-04 15:13:24 -0400

    Add method to retrieve artist's tracks

--- client/src/app/view/view.component.ts
+++ client/src/app/view/view.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
@@ -8,52 +7,208 @@ import { firstValueFrom } from 'rxjs';
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss'],
 })
-export class ViewComponent{
-
+export class ViewComponent {
   constructor(private http: HttpClient) {}
 
   apiKey: string = '846e19279fa31e6d74cad5d88e4a1a1f';
+  lastFmApiUrl = 'http://ws.audioscrobbler.com/2.0/';
   fetchedTracks: any[] = [];
+  userListeningHistory: any[] = [];
+  username: string = 'ramyeow';
+  filteredTracks: string[] = [];
 
-  async getTopTracks(artistName: string) {
+  async getTop100Tracks(artistName: string) {
     const tracksPerPage = 50;
     const totalPages = 2;
-    const lastFmApiUrl = 'http://ws.audioscrobbler.com/2.0/';
+    const desiredTrackCount = 100;
     let allTracks: any[] = [];
 
     try {
+      let fetchedTrackCount = 0;
+
       for (let page = 1; page <= totalPages; page++) {
-        const response = await firstValueFrom(this.http.get<any>(`${lastFmApiUrl}`, {
-          params: {
-            method: 'artist.getTopTracks',
-            artist: encodeURIComponent(artistName),
-            page: page.toString(),
-            api_key: this.apiKey,
-            limit: tracksPerPage.toString(),
-            format: 'json'
-          }
-        }));
+        const response = await firstValueFrom(
+          this.http.get<any>(`${this.lastFmApiUrl}`, {
+            params: {
+              method: 'artist.getTopTracks',
+              artist: artistName,
+              page: page.toString(),
+              api_key: this.apiKey,
+              limit: tracksPerPage.toString(),
+              format: 'json',
+            },
+          })
+        );
 
         if (response.toptracks && response.toptracks.track) {
           for (const track of response.toptracks.track) {
-            if (track.name) {
-              allTracks.push(track.name);
+            if (track.name && fetchedTrackCount < desiredTrackCount) {
+              allTracks.push(track.name.toLowerCase());
+              fetchedTrackCount++;
+
+              if (fetchedTrackCount === desiredTrackCount) {
+                break;
+              }
             }
           }
         }
+      }
 
-    }
-
-    this.fetchedTracks = allTracks;
-
-  }
-    catch (error) {
+      this.fetchedTracks = allTracks;
+      console.log(this.fetchedTracks);
+    } catch (error) {
       console.error('Error fetching top tracks:', error);
     }
   }
 
+  async getArtistTracks(artistName: string) {
+    const tracksPerPage = 100;
+    let page = 1;
+    let allTracks = new Set<string>();
+
+    try {
+      while (true) {
+        const response = await firstValueFrom(
+          this.http.get<any>(`${this.lastFmApiUrl}`, {
+            params: {
+              method: 'artist.getTopTracks',
+              artist: artistName,
+              page: page.toString(),
+              api_key: this.apiKey,
+              limit: tracksPerPage.toString(),
+              format: 'json',
+            },
+          })
+        );
+
+        if (response.toptracks && response.toptracks.track) {
+          for (const track of response.toptracks.track) {
+            if (track.name.toLowerCase()) {
+              allTracks.add(track.name);
+      
+            }
+          }
+        }
+
+        if (response.toptracks.track.length < tracksPerPage) {
+          break;
+        }
+
+        page++;
+      }
+
+      this.filteredTracks = Array.from(allTracks).filter(
+        (track: string) => !this.isLiveOrRemix(track.toLowerCase())
+      );
+
+      console.log('Filtered tracks:', this.filteredTracks);
+      console.log(
+        `Fetched total ${this.filteredTracks.length} unique tracks (after filtering)`
+      );
+    } catch (error) {
+      console.error('Error fetching artist tracks:', error);
+    }
+  }
 
   clickButton() {
-    this.getTopTracks("Madonna");
+    this.getArtistTracks('Lana Del Rey');
+  }
+
+  async getUserListeningHistory(artistName: string) {
+    this.getTop100Tracks(artistName);
+
+    let page = 1;
+    let allTracks: any[] = [];
+    const limit = 200;
+    // const now = Math.floor(Date.now() / 1000);
+    // let lastTimestamp = now - 630720000;
+
+    // console.log('TimeStamp is ', lastTimestamp);
+    try {
+      while (true) {
+        const response = await firstValueFrom(
+          this.http.get<any>(`${this.lastFmApiUrl}`, {
+            params: {
+              method: 'user.getRecentTracks',
+              user: this.username,
+              api_key: this.apiKey,
+              limit: limit.toString(),
+              page: page.toString(),
+              format: 'json',
+            },
+          })
+        );
+
+        if (response.recenttracks && response.recenttracks.track) {
+          for (const track of response.recenttracks.track) {
+            if (
+              track.artist['#text'] === artistName &&
+              this.fetchedTracks.some(
+                (topTrack) =>
+                  topTrack.toLowerCase() === track.name.toLowerCase()
+              )
+            ) {
+              allTracks.push(track);
+              //console.log(`${track.name} is added to the array`);
+            }
+          }
+
+          // if (
+          //   response.recenttracks.track.date &&
+          //   response.recenttracks.track.date.uts
+          // ) {
+          //   lastTimestamp = parseInt(response.recenttracks.track.date.uts);
+          //   console.log(
+          //     `TimeStamp for ${response.recenttracks.track.name} is ${lastTimestamp}.`
+          //   );
+          // }
+
+          if (response.recenttracks.length < limit) {
+            break;
+          }
+        }
+
+        page++;
+
+        this.userListeningHistory = allTracks;
+        //console.log('Complete Listening History:', this.userListeningHistory);
+      }
+    } catch (error) {
+      console.error('Error retrieving recent tracks:', error);
+    }
+  }
+
+  private isLiveOrRemix(trackName: string): boolean {
+    const normalizedTrackName = trackName.toLowerCase();
+    const excludedPatterns = [
+      'acoustic',
+      'remix',
+      'radio edit',
+      'clean version',
+      'live in',
+      'radio mix',
+      'live from',
+      'club mix',
+      '(clean)',
+      'lyrics',
+      '(live)',
+      '[live]',
+      '[explicit]',
+      'remastered',
+      'sped up',
+      '(demo)',
+      '(instrumental)',
+      'demo',
+      '(album mix)',
+      'live at'
+    ];
+
+    return excludedPatterns.some((pattern) =>
+      normalizedTrackName.includes(pattern)
+    );
+  }
+
+  clickButton2() {
+    this.getUserListeningHistory('Peach Pit');
   }
 }
