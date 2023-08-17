03f6e095cdd5a299306d12445c858b460fb09893
ramya1907 <ramya.srinivasan1907@gmail.com>
2023-08-17 13:47:19 -0400

    Add parallax feature to title

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,32 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { LastFmService } from 'src/last-fm.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('parallaxAnimation', [
+      state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(':enter', [style({ opacity: 0, transform: 'translateY(50px)' }), animate('0.5s ease')]),
+    ]),
+  ],
 })
 export class HomeComponent {
+
+  parallaxVisible = false;
+  
   title = 'Home';
 
   username: string = '';
   userExists = true;
   userEntered = true;
 
-  constructor(
-    private router: Router,
-    private lastFmService: LastFmService
-  ) {}
+  constructor(private router: Router, private lastFmService: LastFmService) {}
+
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const scrollTop = event.target.documentElement.scrollTop || event.target.body.scrollTop;
+    console.log('Scroll Top:', scrollTop);
+    const parallaxTriggerPoint = 100; 
+    const contentTriggerPoint = 200; 
+
+    this.parallaxVisible = scrollTop > parallaxTriggerPoint;
+  }
+
 
   checkInput(username: string) {
     if (!username) {
       this.userEntered = false;
+    } else {
+      this.userEntered = true;
     }
-    else 
-      {
-        this.userEntered = true;
-      }
   }
   async checkUsernameAndNavigate() {
     try {
@@ -45,7 +70,7 @@ export class HomeComponent {
     }
   }
 
-  //change view of home screen is user exists 
+  //change view of home screen is user exists
   //give option to change user or sign out
 
   setUsername() {
@@ -61,15 +86,15 @@ export class HomeComponent {
     localStorage.setItem('username', this.username);
   }
 
-  fetchArtistData(){
-    if(this.userExists)
-    {this.router.navigate(['/view']);}  
-    
+  fetchArtistData() {
+    if (this.userExists) {
+      this.router.navigate(['/view']);
+    }
   }
 
-  viewHeatMap(){
-    if(this.userExists)
-    {this.router.navigate(['/heatmap']);}  
-
+  viewHeatMap() {
+    if (this.userExists) {
+      this.router.navigate(['/heatmap']);
+    }
   }
 }
